6ef31c6d956ece2a63121a954cfa3eee7f2e8e0d
Cristian Javier Lopez Ñungo <58754599+CristianJLopez@users.noreply.github.com>
2020-12-04 21:48:17 -0500

    Arreglo error punto 1

--- quiz.js
+++ quiz.js
@@ -1,22 +1,23 @@
-
-	var motor = prompt("ingrese tipo de motor (0-1-2-3 ó 4): ")
-	switch (motor){
-		case 0:
+function tipoMotor(tipo){
+	switch (tipo){
+		case '0':
 			document.write("No hay establecido un valor definido para el tipo de bomba<br>")
-			break
-		case 1:
+			break;
+		case '1':
 			document.write("La bomba es una bomba de agua<br>")
-			break
-		case 2:
+			break;
+		case '2':
 			document.write("La bomba es una bomba de gasolina<br>")
-			break
-		case 3:
+			break;
+		case '3':
 			document.write("La bomba es una bomba de hormigon<br>")
-			break
-		case 4:
+			break;
+		case '4':
 			document.write("La bomba es una bomba de pasta alimenticia<br>")
-			break
+			break;
 		default:
 			document.write("No existe un valor valido para ese tipo de bomba")
 
 	}
+}
+tipoMotor(prompt("ingrese tipo de motor (0-1-2-3 ó 4): ")) 
